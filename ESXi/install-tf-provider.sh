342c812ff61bb369d5c7dcd2487bfec5ba4c9386
Krzysztof Waberski <kwaberski@guardanet.net>
2021-01-24 19:14:07 +0000

    removed sensitive terraform files

---
+++ ESXi/install-tf-provider.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+### Steps to install ESXi terraform provider
+# https://github.com/josenk/terraform-provider-esxi
+sudo apt-get install golang
+mkdir $HOME/go
+export GOPATH="$HOME/go"
+git clone https://github.com/josenk/terraform-provider-esxi.git $GOPATH/src/github.com/terraform-providers/terraform-provider-esxi
+go get -u -v golang.org/x/crypto/ssh
+go get -u -v github.com/hashicorp/terraform
+go get -u -v github.com/josenk/terraform-provider-esxi
+
+cd $GOPATH/src/github.com/josenk/terraform-provider-esxi
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -extldflags "-static"' -o terraform-provider-esxi_`cat version`
+sudo cp terraform-provider-esxi_`cat version` /usr/local/bin
+
+
+# install terraform
+# download terraform_0.12.30_linux_amd64.zip from https://www.terraform.io/downloads.html
+unzip ~/terraform_0.12.30_linux_amd64.zip
+sudo cp terraform /usr/local/bin
